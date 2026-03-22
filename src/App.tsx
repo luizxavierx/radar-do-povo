b71fd984bfb70fabb6bb1a61f0cd29af3925d5b8
jalinbucetou <jalinbucetou@gmail.com>
2026-03-22 06:40:35 -0300

    Reset scroll position on route changes

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AppShellLayout from "@/components/AppShellLayout";
+import ScrollToTop from "@/components/ScrollToTop";
 import Index from "./pages/Index";
 import PoliticoDetalhe from "./pages/PoliticoDetalhe";
 import BuscaPage from "./pages/BuscaPage";
@@ -32,6 +33,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
+        <ScrollToTop />
         <Routes>
           <Route element={<AppShellLayout />}>
             <Route path="/" element={<Index />} />
